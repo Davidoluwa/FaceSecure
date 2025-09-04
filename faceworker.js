4d94b96c13933a1a0725d6e475badf057bb53f9b
Davidoluwa <97314680+Davidoluwa@users.noreply.github.com>
2025-09-04 13:08:30 +0100

    mobile-support optimized 2 faceworker.js

--- faceworker.js
+++ faceworker.js
@@ -4,14 +4,28 @@ self.onmessage = async (e) => {
     const imageData = e.data;
 
     if (!self.modelsLoaded) {
-        await faceapi.nets.tinyFaceDetector.loadFromUri('/FaceSecure/models');
-        await faceapi.nets.faceLandmark68Net.loadFromUri('/FaceSecure/models');
-        await faceapi.nets.faceRecognitionNet.loadFromUri('/FaceSecure/models');
-        self.modelsLoaded = true;
+        try {
+            await Promise.all([
+                faceapi.nets.ssdMobilenetv1.loadFromUri('/FaceSecure/models'),
+                faceapi.nets.faceLandmark68Net.loadFromUri('/FaceSecure/models'),
+                faceapi.nets.faceRecognitionNet.loadFromUri('/FaceSecure/models')
+            ]);
+            self.modelsLoaded = true;
+            console.log('Worker: Models loaded');
+        } catch (error) {
+            console.error('Worker: Error loading models:', error);
+            self.postMessage(null);
+            return;
+        }
     }
 
-    const options = new faceapi.TinyFaceDetectorOptions({ inputSize: 128, scoreThreshold: 0.4 });
-    const detections = await faceapi.detectSingleFace(imageData, options).withFaceLandmarks().withFaceDescriptor();
-
-    self.postMessage(detections);
+    try {
+        const options = new faceapi.SsdMobilenetv1Options({ minConfidence: 0.5 });
+        const detections = await faceapi.detectSingleFace(imageData, options).withFaceLandmarks().withFaceDescriptor();
+        console.log('Worker: Detection result:', detections);
+        self.postMessage(detections);
+    } catch (error) {
+        console.error('Worker: Detection error:', error);
+        self.postMessage(null);
+    }
 };
